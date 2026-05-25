f4052ea96a1b714f04ef182fd5e976f4fd9c6b5d
Omogorioladev <Omogorioladev@gmail.com>
2026-05-25 09:11:54 +0100

    add missing code

---
+++ marine-api/src/Routes/dashboardRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getAdmindashboard } from "../Controllers/dashboardController.js";
+import { authenticateToken } from "../middleware/auth.js";
+import { authorizeRoles } from "../middleware/roles.js";
+
+const router = express.Router();
+
+router.get("/admin", authenticateToken, authorizeRoles("ADMIN"), getAdmindashboard);
+
+export default router;
